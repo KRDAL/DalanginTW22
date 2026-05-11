fa05bdea91158541ecf7e247bc8085936a3b6201
KRDAL <karlodalangin22@gmail.com>
2026-05-11 17:59:06 +0800

    done test

--- PangTestLang/test1.php
+++
@@ -1,9 +0,0 @@
-<?php
-echo "<h1>PHP is working!</h1>";
-echo "<p>Hello from your MacBook and MAMP.</p>";
-
-$name = "Kai";
-echo "<p>Welcome, $name!</p>";
-
-echo "<p>Today is " . date("l, F j, Y") . "</p>";
-?>
