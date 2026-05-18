d6c8f0eca53550dc7a0b1982d9f79e8d5a265eba
KRDAL <karlodalangin22@gmail.com>
2026-05-18 11:49:46 +0800

    di pa tapos mod 4.1

--- Module 4/TFA4.1/story3.php
+++ Module 4/TFA4.1/story3.php
@@ -4,4 +4,4 @@
     <img src="https://picsum.photos/id/1062/400/250" alt="Lion and Mouse" class="story-img">
     <p>A small mouse accidentally woke a sleeping lion. The lion was about to eat him when the mouse begged for mercy, promising to repay the favor one day. Amused, the lion let him go. Later, hunters captured the lion and tied him to a tree. Hearing his roars, the tiny mouse arrived and chewed through the thick ropes, setting the king of the jungle free.</p>
 </div>
-<?php include 'footer.php'; ?>
+<?php include 'footer.php'; ?>f
